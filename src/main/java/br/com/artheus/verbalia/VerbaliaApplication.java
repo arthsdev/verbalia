9715e2e9f50265e706f2c5a8ffb26d7d530b5890
Arths <artheusdev@gmail.com>
2025-07-09 04:36:34 -0300

    chore: initialize Spring Boot project with basic dependencies

---
+++ src/main/java/br/com/artheus/verbalia/VerbaliaApplication.java
@@ -0,0 +1,13 @@
+package br.com.artheus.verbalia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbaliaApplication.class, args);
+	}
+
+}
